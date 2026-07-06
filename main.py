26b62a34994063842916e7fc13df2ae44ed4ea2e
GustavoMenezes-Hub <gustavomenezess578@gmail.com>
2026-07-06 15:37:19 -0300

    Commit do main

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-# ponto de entrada, criaremos a janela e chamaremos as funções
+#From formas import
+
+from tkinter import *
+from tkinter import ttk
+
+# ponto de entrada, criaremos a janela e chamaremos as funcoes
+
+figuras = []       # Todas as figuras desenhadas
+figura_nova = None # Figura que esta sendo desenhada, mas ainda nao foi incluida em figuras
+
+root = Tk()
+frame = Frame(root)
+
+# Widgets arranjados com Layout grid dentro de frame
+paddings = {'padx': 5, 'pady': 5} 
+
+# label
+label = ttk.Label(frame,  text='Linha ou Rabisco:')
+label.grid(column=0, row=0, sticky=W, **paddings)
+
+# option menu
+tipo_figura_var = StringVar(root) # Guarda o tipo de figura selecionado no option menu (linha ou rabisco)
+option_menu = ttk.OptionMenu(frame, tipo_figura_var,
+                             'Linha', 'Linha', 'Rabisco', 'Circulo')
+option_menu.grid(column=1, row=0, sticky=W, **paddings)
+
+# Area de desenho
+canvas = Canvas(frame, bg='white', width=600, height=600)
+canvas.grid(column=0, row=1, columnspan=2, sticky=W, **paddings)
+
+frame.pack()
+
+# Eventos de mouse associados ao canvas - com seus callbacks
+canvas.bind('<ButtonPress-1>', iniciar_figura_nova)
+canvas.bind('<B1-Motion>', atualizar_figura_nova)
+canvas.bind('<ButtonRelease-1>', incluir_figura_nova)
+
+root.mainloop()
